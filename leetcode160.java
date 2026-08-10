a96f10ec86c3a52debbdbadcd33cfa12ff65aa6c
Linsen-lsjs <1242654668@qq.com>
2026-08-10 18:01:11 +0800

    题号：160

---
+++ leetcode160.java
@@ -0,0 +1,56 @@
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class leetcode160 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode cura = headA;
+        ListNode curb = headB;
+        int counta = 0;
+        int countb = 0;
+
+        while (cura != null) {
+            counta++;
+            cura = cura.next;
+        }
+
+        while (curb != null) {
+            countb++;
+            curb = curb.next;
+        }
+
+        int c = counta - countb;
+
+        if (c < 0) {
+            while (c != 0) {
+                headB = headB.next;
+                c++;
+            }
+        } else {
+            while (c != 0) {
+                headA = headA.next;
+                c--;
+            }
+
+        }
+
+        while (headA != null) {
+
+            if (headA == headB) {
+                break;
+            }
+
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+}
